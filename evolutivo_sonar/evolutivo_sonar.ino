dd4559a916f81ccfff3b0a87b4afae520effb207
planetamongoloid <planetamongoloid@gmail.com>
2014-12-12 13:02:28 -0600

    Cambios realizado en el sitio para limitar la zona hasta donde llega el sensor

--- evolutivo_sonar/evolutivo_sonar.ino
+++ evolutivo_sonar/evolutivo_sonar.ino
@@ -22,6 +22,7 @@ char buf[12];
 float valor_array[10] = {
   290,290,290,290,290,290,290,290,290,290};
 long cm_last;
+long error;
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {  
@@ -36,6 +37,7 @@ unsigned int localPort = 8888;      // local port to listen on
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 char  ReplyBuffer[] = "acknowledged";       // a string to send back
 
+int VENTANA = 13;
 // An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
 
@@ -66,11 +68,17 @@ void loop() {
   pinMode(pingPin, INPUT);
   duration = pulseIn(pingPin, HIGH);
   cm = microsecondsToCentimeters(duration);
+  
+  error= cm;
+  // hago un map porque hemos tenido que reducir el recorrido del carril tenemos menos longitud
+  // el valor 200 se podria hacer variable, incluir un potenciometro?
+  cm = map(cm,5,143,0,290);
+  
   valor_array[0] = valor_array[1];
   valor_array[1] = valor_array[2];
   valor_array[2] = valor_array[3];
   valor_array[3] = valor_array[4];
-  
+
   valor_array[4] = valor_array[5];
   valor_array[5] = valor_array[6];
   valor_array[6] = valor_array[7];
@@ -78,13 +86,22 @@ void loop() {
   valor_array[8] = valor_array[9];
   // guardo en la ultima posicion
   valor_array[9] = cm;
+  // check de el valor anterior , le pongo una ventana de valores para evitar flicket
+  // en el inicio hay que llevar la pantalla hasta el principio porque empieza el array con valor 290
+  // si cuando arrancas el mac esta la pantalla en el medio obviamente el valor es mayor que la ventana y siempre quedaria en 290
+  if(valor_array[9] > valor_array[8]+VENTANA || valor_array[9] < valor_array[8]-VENTANA){
+      
+    valor_array[9] = valor_array[8];
+  }
   cm = round((valor_array[0] + valor_array[1] + valor_array[2] + valor_array[3] + valor_array[4] + valor_array[5] + valor_array[6] + valor_array[7] + valor_array[8] + valor_array[9]) / 10);
   //Serial.println("enviando");
-  //cm = map(cm,3,290,100,0);
-  
+ 
+
   Serial.print(cm);
   Serial.print("cm--");
-  
+  Serial.print(error);
+  Serial.print("++++");
+
   Serial.print(valor_array[0]);
   Serial.print(",");
   Serial.print(valor_array[1]);
@@ -104,15 +121,15 @@ void loop() {
   Serial.print(valor_array[8]);
   Serial.print(",");
   Serial.print(valor_array[9]);
-  
+
   Serial.println();
   if(cm != cm_last){
     cm_last = cm;
-  // send a reply, to the IP address and port that sent us the packet we received
-  Udp.beginPacket(remoto, remotoPort);
-  Udp.write(itoa(cm,buf,10));
-  //Udp.write("5");
-  Udp.endPacket();
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(remoto, remotoPort);
+    Udp.write(itoa(cm,buf,10));
+    //Udp.write("5");
+    Udp.endPacket();
   }
 
   delay(50);
@@ -124,6 +141,8 @@ long microsecondsToCentimeters(long microseconds)
   // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
+
+
 
 
 
